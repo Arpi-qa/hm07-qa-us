f13e6df4fcae8c96c104ef41fa36a796f02b6ef3
Arpi <keshishyan0202@gmail.com>
2023-10-22 20:37:23 -0700

    readme.md

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://d3e03df5-a8f6-4a48-9c7b-a8aac58117df.serverhub.tripleten-services.com"
+    API_URL: "https://78834ffd-7637-46d0-b97a-17e525cde684.serverhub.tripleten-services.com"
 }
